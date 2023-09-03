a8678dcabc4134ecd82905f5b067718218fabe34
Taylor Bisset <taylorbisset@gmail.com>
2023-09-03 11:26:49 -0700

    Program complete

--- CS131-zybooks-Program1-PhoneNumberBreakdown/CS131-zybooks-Program1-PhoneNumberBreakdown.cpp
+++ CS131-zybooks-Program1-PhoneNumberBreakdown/CS131-zybooks-Program1-PhoneNumberBreakdown.cpp
@@ -5,3 +5,17 @@ int main()
     std::cout << "Hello World!\n";
 }
 
+/*
+Given a string representing a 10-digit phone number, output the area code, prefix, 
+and line number using the format (800) 555-1212.
+
+Ex: If the input is:
+
+8005551212
+the output is:
+
+(800) 555-1212
+Hint: Use the modulo and division operations.
+
+For simplicity, assume all phone numbers are 10-digit. So 18005551212 is not allowed.
+*/
